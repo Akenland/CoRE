fc24b23ab846b619ddb126cbbd54fcad2249c24c
Kyle Nanakdewa <KyleN@live.ca>
2017-10-19 18:20:17 -0400

    cleaned up, added javadoc, redid constructors

--- src/com/KyleNecrowolf/RealmsCore/Prompts/Prompt.java
+++ src/com/KyleNecrowolf/RealmsCore/Prompts/Prompt.java
@@ -1,22 +1,22 @@
 package com.KyleNecrowolf.RealmsCore.Prompts;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 
 import com.KyleNecrowolf.RealmsCore.Common.ConfigAccessor;
 import com.KyleNecrowolf.RealmsCore.Common.Utils;
 
 /**
- * A prompt that can be displayed to CommandSenders, showing them "questions" and allowing them to choose answers.
+ * A prompt that can be displayed to a {@link CommandSender}, showing them questions and allowing them to choose answers.
  */
 public class Prompt {
 
@@ -27,9 +27,6 @@ public class Prompt {
 
 
 	//// Prompt Data
-	private final String conversationName;
-	private final String promptName;
-
 	private List<String> questions;
 	private boolean randomQuestions;
 	private List<String> answers;
@@ -39,7 +36,8 @@ public class Prompt {
 
 	//// Constructor
 	/**
-	 * Creates a prompt from a ConfigurationSection.
+	 * Loads a prompt from a {@link ConfigurationSection}.
+	 * @param config the {@link ConfigurationSection} to load prompt data from
 	 */
 	public Prompt(ConfigurationSection config){
 		// Load the data from file
@@ -55,68 +53,55 @@ public class Prompt {
 	 * @param promptName the name of the prompt within that file
 	 */
 	public Prompt(String fileName, String promptName){
-		// Save the information in this object
 		fileName = fileName.toLowerCase();
 		promptName = promptName.toLowerCase();
+		ConfigurationSection file;
 
-		// If conversation name is tag, load tag file
-		if(this.conversationName.startsWith("tag:")){
-			String tagName = this.conversationName.replace("tag:", "");
-			// TODO
-			return;
+		// If fileName begins with tag, load tag file
+		Plugin realmsStory = Bukkit.getPluginManager().getPlugin("RealmsStory");
+		if(fileName.startsWith("tag:") && realmsStory!=null){
+			String tagName = fileName.replace("tag:", "");
+			file = new ConfigAccessor("tags\\"+tagName+".yml", realmsStory).getConfig().getConfigurationSection("prompts");
 		}
 
-		// Load the specified conversation file
-		FileConfiguration conversationFile = new ConfigAccessor("prompts\\"+fileName+".yml").getConfig();
-		if (conversationFile.contains(this.conversationName + "." + this.promptName)) {
+		// Else, load from RealmsCore prompts folder
+		else{
+			file = new ConfigAccessor("prompts\\"+fileName+".yml").getConfig().getConfigurationSection(fileName);
+		}
+		
+		file = file.getConfigurationSection(promptName);
+		if(file!=null){
 			// Load the data from file
-			this.questions = conversationFile.getStringList(this.conversationName + "." + this.promptName + ".question");
-			this.randomQuestions = conversationFile.getBoolean(this.conversationName + "." + this.promptName + ".randomQuestion");
-			this.answers = conversationFile.getStringList(this.conversationName + "." + this.promptName + ".answers");
-			this.actions = conversationFile.getStringList(this.conversationName + "." + this.promptName + ".actions");
-			this.conditions = conversationFile.getStringList(this.conversationName + "." + this.promptName + ".conditions");
+			this.questions = file.getStringList("questions");
+			this.randomQuestions = file.getBoolean("randomQuestions");
+			this.answers = file.getStringList("answers");
+			this.actions = file.getStringList("actions");
+			this.conditions = file.getStringList("conditions");
+		} else {
+			Utils.notifyAdminsError("Could not find prompt "+fileName+"."+promptName);
 		}
 	}
-	
-	// Create a prompt from a file, with the combined name (conversationName.promptName)
+	/**
+	 * Loads a prompt from RealmsCore's prompts folder. Prefixing the fileName with "tag:" will look in RealmsStory tags instead.
+	 * @param combinedName the name of the file and prompt, in the format fileName.promptName
+	 * @see Prompt#Prompt(String, String)
+	 */
 	public Prompt(String combinedName){
 		this(combinedName.split("\\.",2)[0], combinedName.split("\\.",2)[1]);
 	}
-	
-	
-	// Create a prompt by suppling the values directly
-	public Prompt(List<String> questions, List<String> questionConditions, boolean randomQuestion, List<String> answers, List<String> actions, List<String> answerConditions){
-		this.conversationName = "generated";
-		this.promptName = "temp";
-		this.questions = questions;
-		this.randomQuestions = randomQuestion;
-		this.answers = answers;
-		this.actions = actions;
-		this.conditions = answerConditions;
-		}
-	public Prompt(List<String> question, List<String> answers, List<String> actions, List<String> conditions){
-		this.conversationName = "generated";
-		this.promptName = "temp";
-		this.questions = question;
-		this.answers = answers;
-		this.actions = actions;
-		this.conditions = conditions;
-	}
-	public Prompt(String question, List<String> answers, List<String> actions, List<String> conditions){
-		this(Arrays.asList(question), answers, actions, conditions);
-	}
-
 	/**
 	 * Creates an empty prompt. Data must be added before this prompt can be displayed.
 	 */
 	public Prompt(){
-		this(new ArrayList<String>(), new ArrayList<String>(), new ArrayList<String>(), new ArrayList<String>());
+		this.questions = new ArrayList<String>();
+		this.answers = new ArrayList<String>();
+		this.actions = new ArrayList<String>();
+		this.conditions = new ArrayList<String>();
 	}
 
 
 
 	//// Prompt Editing methods
-
 	/**
 	 * Adds a question to this Prompt.
 	 * @param question the question string to add
@@ -136,6 +121,11 @@ public class Prompt {
 		actions.add(action);
 		conditions.add(condition);
 	}
+	/**
+	 * Adds an answer to this Prompt.
+	 * @param answer the answer string to add
+	 * @param action the action to run when this answer is clicked
+	 */
 	public void addAnswer(String answer, String action){
 		addAnswer(answer, action, null);
 	}
@@ -145,37 +135,29 @@ public class Prompt {
 
 	/**
 	 * Displays this prompt to a {@link CommandSender}.
-	 * @param sender the CommandSender to display to
+	 * @param sender the {@link CommandSender} to display to
 	 * @param prefix an optional prefix for all questions
 	 */
-	public void display(CommandSender sender, String prefix) {
+	public void display(CommandSender sender, String prefix){
 		// If not loaded, show an error and stop
 		if(questions==null || questions.isEmpty()){
 			Utils.sendActionBar(sender, Utils.errorText+"Prompt not found. Ask an Admin for help.");
-			Utils.notifyAdminsError("Could not find prompt "+conversationName+"."+promptName+" to display to "+sender.getName());
+			Utils.notifyAdminsError("Could not find prompt to display to "+sender.getName());
 			return;
 		}
-		
+
+		// Make the prefix gray
+		prefix = ChatColor.GRAY + prefix + ChatColor.GRAY;
+
 		// If randomQuestion, pick one question line randomly
 		if(randomQuestions){
 			String questionLine = questions.get(ThreadLocalRandom.current().nextInt(questions.size()-1));
-			sender.sendMessage(Utils.messageText + prefix + Utils.messageText + ChatColor.translateAlternateColorCodes('&', questionLine));
-		} else {
-			// Otherwise, display each line of the question (delayed slightly)
-			//int delay = 0;
-			for (String questionLine : questions) {
-				String message = Utils.messageText + prefix + Utils.messageText + ChatColor.translateAlternateColorCodes('&', questionLine);
-				sender.sendMessage(message);
-				/*new BukkitRunnable(){
-					@Override
-					public void run(){
-						sender.sendMessage(message);
-					}
-				}.runTaskLater(Main.getProvidingPlugin(Main.class), delay);
-				delay += 10;*/
-			}
+			sender.sendMessage(prefix + ChatColor.translateAlternateColorCodes('&', questionLine));
 		}
-		
+		// Otherwise, display each line of the question
+		else for (String questionLine : questions) sender.sendMessage(prefix + ChatColor.translateAlternateColorCodes('&', questionLine));
+
+
 		// If there are answers, prompt the player and list the options
 		if (!answers.isEmpty() && sender instanceof Player){
 
@@ -234,7 +216,11 @@ public class Prompt {
 		}
 	}
 
-	public void display(CommandSender sender) {
+	/**
+	 * Displays this prompt to a {@link CommandSender}.
+	 * @param sender the {@link CommandSender} to display to
+	 */
+	public void display(CommandSender sender){
 		display(sender, "");
 	}
 }
