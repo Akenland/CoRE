fdfbfc477f7f370b888ede64a58dcbdcef313745
Kyle Nanakdewa <KyleN@live.ca>
2021-02-16 19:02:19 -0500

    improve relevancy of tabcompletes for item command

--- src/com/kylenanakdewa/core/Extras/ItemCommands.java
+++ src/com/kylenanakdewa/core/Extras/ItemCommands.java
@@ -15,47 +15,54 @@ import com.kylenanakdewa.core.common.Utils;
 
 public final class ItemCommands implements TabExecutor {
 
-    //// Commands
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		// Check permissions
-		if(!sender.hasPermission("core.item")) return Error.NO_PERMISSION.displayChat(sender);
+		if (!sender.hasPermission("core.item"))
+			return Error.NO_PERMISSION.displayChat(sender);
 
 		// If no args or sender is not player, return error
-		if(args.length==0 || !(sender instanceof Player)) return Error.INVALID_ARGS.displayActionBar(sender);
-
+		if (args.length == 0 || !(sender instanceof Player))
+			return Error.INVALID_ARGS.displayActionBar(sender);
 
 		// Item data
 		final Material material = Material.matchMaterial(args[0]);
 		int amount = 0;
-		short damage = 0;
-		try{
-			amount = (args.length>=2) ? Integer.parseInt(args[1]) : 1;
-			damage = (args.length>=3) ? Short.parseShort(args[2]) : 0;
-		} catch(NumberFormatException e){}
+		try {
+			amount = (args.length >= 2) ? Integer.parseInt(args[1]) : 1;
+		} catch (NumberFormatException e) {
+		}
 
 		// Make sure material is not null
-		if(material==null) return Error.ITEM_NOT_FOUND.displayActionBar(sender);
+		if (material == null)
+			return Error.ITEM_NOT_FOUND.displayActionBar(sender);
 		// Make sure amount is greater than 0
-		if(amount<=0 || damage<0) return Error.INVALID_ARGS.displayActionBar(sender);
+		if (amount <= 0)
+			return Error.INVALID_ARGS.displayActionBar(sender);
 
 		// Give item to player
-		((Player)sender).getInventory().addItem(new ItemStack(material, amount, damage));
-		Utils.sendActionBar(sender, amount+" "+material.name().replace("_", " ").toLowerCase()+" added to inventory");
+		((Player) sender).getInventory().addItem(new ItemStack(material, amount));
+		Utils.sendActionBar(sender,
+				amount + " " + material.name().replace("_", " ").toLowerCase() + " added to inventory");
 		return true;
 	}
 
-
-	//// Tab completions
 	@Override
 	public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
-		if(args.length==1 && sender.hasPermission("core.item")){
+		if (args.length == 1 && sender.hasPermission("core.item")) {
 			// Add all item names as completion
 			List<String> completions = new ArrayList<String>();
-			for(Material type : Material.values()) completions.add(type.name());
+			for (Material type : Material.values())
+				completions.add(type.name());
+
+			// Sort by relevancy
+			String materialArg = args[0];
+			completions.sort((material1, material2) -> material1.contains(materialArg) ? -1
+					: material2.contains(materialArg) ? 1 : 0);
+
 			return completions;
 		}
-		
+
 		return null;
 	}
 }
